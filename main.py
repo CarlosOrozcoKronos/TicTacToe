f658d53fd425511260fbaf7162a89010a8a75ca1
KRONO\Carlos <adan_alda@hotmail.com>
2021-05-27 20:30:03 +0200

    definidas clases principales y parte del tablero

---
+++ main.py
@@ -0,0 +1,36 @@
+import os   
+
+# CONTANTES
+x = 1
+O = -1
+BLANCK = 0
+
+#Classes
+
+class Tablero:
+    """
+    muestra el tablero, que contiene las fichas, puede devolver la posicion de las mismas
+    """
+    def __init__(self):
+        self.tablero = [[[BLANCK],[BLANCK],[BLANCK]],
+                [[BLANCK],[BLANCK],[BLANCK]],
+                [[BLANCK],[BLANCK],[BLANCK]]]
+    
+    def insertarFicha(self, ficha, posicion)
+        """
+        ficha = X o O
+        posicion numero de 1 a 9
+        """
+        pass
+
+
+class Jugador:
+    """
+    """
+    pass
+
+
+class Juego:
+    """
+    """
+    pass
