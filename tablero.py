e156deb615333d1c3b5780dcac621fc585034ce1
Carlos Orozco <adan_alda@hotmail.com>
2021-06-02 18:59:39 +0200

    tablero; implementadas formas de obtener la posicion, intercambiar entre el simbolo y el valor de las fichas, mostrar de forma rudimentaria el tablero

--- tablero.py
+++ tablero.py
@@ -1,34 +1,58 @@
-# CONTANTES
-x = 1
+# CONSTANTES
 O = -1
 BLANCK = 0
+x = 1
+FICHAS = ["O", " ", "X"]
+
 
 class Tablero:
     """
-    muestra el tablero, que contiene las fichas, puede devolver la posicion de las mismas
+    muestra el tablero, que contiene las fichas, puede devolver la posicion de
+    las mismas
+    123
+    456
+    789
     """
 
-
     def __init__(self):
-        self.tablero = [[[BLANCK],[BLANCK],[BLANCK]],
-                [[BLANCK],[BLANCK],[BLANCK]],
-                [[BLANCK],[BLANCK],[BLANCK]]]
-    
+        self.tablero = [[BLANCK, BLANCK, BLANCK],
+                        [BLANCK, BLANCK, BLANCK],
+                        [BLANCK, BLANCK, BLANCK]]
+
+    def getXYDePosicion(self, posicion):
+        posx = (posicion % 3) - 1
+        if posx == -1:
+            posx = 2
+        posy = 0
+        if posicion >= 7:
+            posy = 2
+        elif posicion >= 4:
+            posy = 1
+        return (posy, posx)
+
+    def getPosicionDeXY(self, posX, posY):
+        posicion = (posX + 1) + posY * 3
+        return posicion
+
     def insertarFicha(self, ficha, posicion):
         """
         ficha = X o O
         posicion numero de 1 a 9
         """
-        pass
+        YX = self.getXYDePosicion(posicion)
+        self.tablero[YX[0]][YX[1]] = self.traducirFicha(ficha)
 
+    def traducirFicha(self, ficha):
+        """
+        devuelve el valor numerico de la ficha o su simbolo
+        """
+        if ficha in FICHAS:
+            return (FICHAS.index(ficha) - 1)
+        elif (ficha + 1) in range(len(FICHAS)):
+            return FICHAS[ficha + 1]
+        raise Exception("Ficha no valida")
 
     def mostrarTablero(self):
-        pass
-
-
-    def setFicha(self):
-        pass
-
-
-    def getTablero(self):
-        return self.tablero
+        print()
+        for linea in self.tablero:
+            print(f"{linea[0]}|{linea[1]}|{linea[2]}")
