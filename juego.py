611c9198edde6fb3333ac6f275ab3a5770842b4a
Carlos Orozco <adan_alda@hotmail.com>
2021-06-02 20:04:19 +0200

    el jugador ahora puede insertar fichas en el tablero: tablero.insertarFicha("X", jugador1.jugada(turno, juego , tablero))

--- juego.py
+++ juego.py
@@ -3,6 +3,9 @@ class Juego:
     def __init__(self):
         self.turno = 0
 
+    def jugadaEsValida(self, jugada, vacias):
+        return str(jugada).isdigit() and int(jugada) in vacias
+
     def pedirJugada(self):
         pass
 
--- jugador.py
+++ jugador.py
@@ -1,7 +1,23 @@
+import juego
+import tablero
+
+
 class Jugador:
 
     def __init__(self):
         pass
 
-    def jugada(self):
-        pass
+    def jugada(self, turno, juego, tablero):
+        """
+        juego es un objero de tipo juego, tablero es de tipo tablero
+        """
+        print(f"Es el turno de jugador :{turno},")
+        print("introduce en que casilla quieres tu ficha")
+        vacias = tablero.getCasillasVacias()
+        print(f"solo quedan las casillas {vacias}")
+        miJugada = input("123\r\n456\r\n789")
+        while (not juego.jugadaEsValida(miJugada, vacias)):
+            print("has introducido una jugada no valida, introduce:")
+            print(f"recuerda que solo quedan las casillas {vacias}")
+            miJugada = input("123\r\n456\r\n789")
+        return miJugada
